7f8f3099e665b8f6b437ec6d16666981f47dfa15
Leandro Silva <leandroasilva290@gmail.com>
2018-10-23 17:48:41 -0300

    Versao quase quem sabe funcional

--- src/OpenCV/code.cpp
+++ src/OpenCV/code.cpp
@@ -21,26 +21,19 @@ int iLowV = 0;
 int iHighV = 255;
 void select_Channel(Mat&, Mat&, int, int);
 
-int right_x;
-void histogram_Right(Mat&);
-
 /* Variáveis das funções de sliding_Window */
-int h_rectangle = 25;
-int l_rectangle = 50;
-int num_rectangle = 12;
+int h_rectangle = 50;
+int l_rectangle = 100;
+int num_rectangle = 20;
 
-vector<Point2f> right_Line;
-vector<Point2i> iright_Line;
-void sliding_Window_Right(Mat&, Mat&);
+Point2i button;
+int previous_x = -1;
+void histogram_Line(Mat&);
 
-Point2i button_left;
-int previous_left_x = -1;
-void histogram_Left(Mat&);
-
-vector<Point2f> left_Line;
-vector<Point2i> ileft_Line;
-int control_left[20];
-void sliding_Window_Left(Mat&, Mat&);
+vector<Point2f> Central_Line;
+vector<Point2i> iCentral_Line;
+int control_line[20];
+void sliding_Window_Line(Mat&, Mat&);
 
 vector<Point2f> navegavel;
 vector<Point2i> navegavel_int;
@@ -50,9 +43,6 @@ vector<Point2f> final_navegavel;
 vector<Point2i> final_navegavel_int;
 void inverse_bird_Eyes(Mat&, Mat&, Mat&);
 void inverse_bird_Eyes_2(Mat&, Mat&, Mat&);
-
-vector <Point2i> central_line;
-void central_Line(Mat&);
 
 float shift_Central(Mat&);
 
@@ -80,52 +70,37 @@ int main( int argc, char** argv )
     namedWindow(result_window, CV_WINDOW_NORMAL);
 
     for(int j = 0; j<num_rectangle; j++)
-        control_left[j] = -1;
+        control_line[j] = -1;
 
     while ( (waitKey(33) != 27) && (cap.isOpened()) )
     {
         cap.read(src);
 
         //bird_Eyes_2(src, 40, 90, 90, 430, 600, bird_img);
-        bird_Eyes(src, bird_img);
 
-        //bird_img = src;
+        bird_img = src;
 
         select_Channel(bird_img, color_img, iLowV, iHighV);
-
-        histogram_Right(color_img);
-
-        sliding_img = bird_img.clone();
-        sliding_Window_Right(color_img, sliding_img);
         
-        histogram_Left(color_img);
+        histogram_Line(color_img);
 
-        sliding_Window_Left(color_img, sliding_img);
+        sliding_img = src.clone();
 
-        navegation_Zone(sliding_img,navegation_img);
+        sliding_Window_Line(color_img, sliding_img);
 
         //inverse_bird_Eyes(src, bird_img, result_img);
 
         //inverse_bird_Eyes_2(src, bird_img, result_img);
 
-        central_Line(result_img);
-
-        shift_Central(result_img);
+        //shift_Central(result_img);
 
         imshow(source_window, src);
-        imshow(result_window, sliding_img);
+        //imshow(result_window, sliding_img);
 
-        right_Line.clear();
-        iright_Line.clear();
-        left_Line.clear();
-        ileft_Line.clear();
-        navegavel.clear();
-        navegavel_int.clear();
-        final_navegavel.clear();
-        final_navegavel_int.clear();
-        central_line.clear();
+        Central_Line.clear();
+        iCentral_Line.clear();
 
-        //waitKey(0);
+        waitKey(0);
     }
 
     waitKey(0);
@@ -143,12 +118,6 @@ void bird_Eyes(Mat& in, Mat& out)
     src_vertices[1] = Point(0.30*Cols, 0.20*Rows); // 
     src_vertices[2] = Point(0.70*Cols, 0.20*Rows); // 
     src_vertices[3] = Point(     Cols, 0.90*Rows); // 
-
-    /*Mat draw(in.rows, in.cols, CV_8UC3, Scalar(0,0,0) );
-    fillPoly(draw, src_vertices, Scalar (255, 0, 0), 8);
-    char* draw_window = "Regiao Navegavel";
-    namedWindow(draw_window, CV_WINDOW_NORMAL);
-    imshow(draw_window, draw);*/
 
     Point2f dst_vertices[4];
     dst_vertices[0] = Point(  0, 480);
@@ -282,7 +251,7 @@ void select_Channel(Mat& in, Mat& out, int low, int high)
     imshow(color_window, out);
 }
 
-void histogram_Right(Mat& in)
+void histogram_Line(Mat& in)
 {
     Mat histImage(in.rows, in.cols, CV_8UC3, Scalar(255,255,255) );
 
@@ -290,121 +259,16 @@ void histogram_Right(Mat& in)
     int count_Num = 0;
     int count_Den = 0;
     //int normalized_count[in.cols];
-    float inicial_Row = in.rows * 0.9;
+    float inicial_Row = 0;
+    //float shift_Row = in.rows * 0.1;
 
-    for (int col = (in.cols)/2; col < in.cols; ++col)
-    {
-        count[col]=0;
-        for (int row = inicial_Row; row < in.rows; ++row)
-        {
-            if ( (int)(in.at<uchar>(row,col)) != 0)
-            {
-                ++count[col];
-                count_Num+=(col*count[col]);
-                count_Den+=count[col];
-            }
-                
-        }
-        //cout<<"A coluna  " << col << "  tem   " << count[col] << endl;
-        //normalize(count[col], normalized_count[col], 0, in.rows, NORM_MINMAX, -1, Mat() );
-        circle(histImage, Point(col, in.rows - count[col]),1,Scalar(0,255,255),1,8,0);
-    }
-
-    if (count_Den != 0)
-        right_x = count_Num / count_Den;
-    
-
-    /*cout << " X da linha direita " << right_x << endl;
-    waitKey(0);*/
-
-    /*char* histogram_window = "Histogram";
-    namedWindow(histogram_window, CV_WINDOW_NORMAL);
-    imshow(histogram_window, histImage);*/
-}
-
-void sliding_Window_Right(Mat& in, Mat& out)
-{
-    // Desenha o quadrado inicial resultante do histograma
-    int y_Reference = in.rows;
-    Point2d initial_Center = Point(right_x, y_Reference);
-    Point2d final_Center;
-
-    Point P1, P2; // pontos auxiliares para desenho dos retângulos
-    P1.x = right_x - (l_rectangle/2);
-    P1.y = y_Reference - (h_rectangle/2);
-    P2.x = right_x + (l_rectangle/2);
-    P2.y = y_Reference;
-    rectangle(out, P1, P2, Scalar(255,255,0), 2, 8, 0);
-
-    Point2d previous_Center = initial_Center;
-    Point2d new_Center;
-    int count[l_rectangle];
-    int count_Num;
-    int count_Den;
-
-    for (int i=0; i<num_rectangle; i++)
-    {
-        count_Num = 0;
-        count_Den = 0;
-
-        new_Center.x = previous_Center.x;
-        new_Center.y = previous_Center.y - (h_rectangle/2);
-
-        P1.x = new_Center.x - (l_rectangle/2);
-        P1.y = new_Center.y - (h_rectangle/2);
-        P2.x = new_Center.x + (l_rectangle/2);
-        P2.y = new_Center.y;
-
-        for (int col = P1.x ; col <= (P1.x + l_rectangle); col++)
-        {
-            count[col-P1.x] = 0;
-            for (int row = P1.y ; row <= (P1.y + h_rectangle); row++)
-            {
-                if ( (int)(in.at<uchar>(row,col)) != 0)
-                {
-                    ++count[col-P1.x];
-                    count_Num+=(col*count[col-P1.x]);
-                    count_Den+=count[col-P1.x];
-                } 
-            }
-            //cout << "A coluna " << col << "(indice " << col-P1.x << " ) tem  " << count[col-P1.x] << endl;
-        }
-
-        if (count_Den != 0)
-            new_Center.x = count_Num / count_Den;
-
-        P1.x = new_Center.x - (l_rectangle/2);
-        P1.y = new_Center.y - (h_rectangle/2);
-        P2.x = new_Center.x + (l_rectangle/2);
-        P2.y = new_Center.y;
-
-        right_Line.push_back(new_Center);
-        iright_Line.push_back(new_Center);
-
-        rectangle(out, P1, P2, Scalar(0,0,255), 2, 8, 0);
-
-        previous_Center = new_Center;
-    }
-}
-
-void histogram_Left(Mat& in)
-{
-    Mat histImage(in.rows, in.cols, CV_8UC3, Scalar(255,255,255) );
-
-    int count[in.cols];
-    int count_Num = 0;
-    int count_Den = 0;
-    //int normalized_count[in.cols];
-    float inicial_Row = in.rows * 0.9;
-    float shift_Row = in.rows * 0.1;
-
-    for (int col = 0; col < (in.cols)/2; ++col)
+    for (int col = (in.cols)/3; col < (in.cols); ++col)
     {
         count[col]=0;
 
-        for (int i=0; (i<10) && (count_Den < 10); i++)
-        {
-            for (int row = inicial_Row - (i*shift_Row); row < in.rows - (i*shift_Row); ++row)
+        //for (int i=0; (i<10) && (count_Den < 10); i++)
+        //{
+            for (int row = inicial_Row ; row < in.rows; ++row)
             {
                 if ( (int)(in.at<uchar>(row,col)) != 0)
                 {
@@ -413,38 +277,40 @@ void histogram_Left(Mat& in)
                     count_Den+=count[col];
                 }
             }
-        }
-        circle(histImage, Point(col, in.rows - count[col]),1,Scalar(0,255,255),1,8,0);
+        //}
+        circle(histImage, Point(col, in.rows - count[col]),1,Scalar(0,0,255),1,8,0);
+
     }
 
-    button_left.y = inicial_Row + shift_Row;
+    button.y = in.rows ;
 
     //normalize(count[col], normalized_count[col], 0, in.rows, NORM_MINMAX, -1, Mat() );
 
-    if ( ( (count_Den > 10) || (previous_left_x == -1))  && (count_Den != 0) )
+    if ( ( (count_Den > 10) || (previous_x == -1))  && (count_Den != 0) )
     {
-        button_left.x = count_Num / count_Den;
-        previous_left_x = button_left.x;
+        button.x = count_Num / count_Den;
+        previous_x = button.x;
     }
     else   
-        button_left.x = previous_left_x;
+        button.x = previous_x;
+    cout << "Ponto inicial" << button.x << "," << button.y << endl;
 
-    /*char* histogram_window = "Histogram 2";
+    char* histogram_window = "Histogram 2";
     namedWindow(histogram_window, CV_WINDOW_NORMAL);
-    imshow(histogram_window, histImage);*/
+    imshow(histogram_window, histImage);
 }
 
-void sliding_Window_Left(Mat& in, Mat& out)
+void sliding_Window_Line(Mat& in, Mat& out)
 {
     // Desenha o quadrado inicial resultante do histograma
-    Point2d initial_Center = Point(button_left.x, button_left.y);
+    Point2d initial_Center = Point(button.x, button.y);
     Point2d final_Center;
 
     Point P1, P2; // pontos auxiliares para desenho dos retângulos
-    P1.x = button_left.x - (l_rectangle/2);
-    P1.y = button_left.y - (h_rectangle/2);
-    P2.x = button_left.x + (l_rectangle/2);
-    P2.y = button_left.y;
+    P1.x = button.x - (l_rectangle/2);
+    P1.y = button.y - (h_rectangle/2);
+    P2.x = button.x + (l_rectangle/2);
+    P2.y = button.y;
     rectangle(out, P1, P2, Scalar(255,255,0), 2, 8, 0);
 
     Point2d previous_Center = initial_Center;
@@ -478,26 +344,26 @@ void sliding_Window_Left(Mat& in, Mat& out)
                     count_Den+=count[col-P1.x];
                 } 
             }
-            //cout << "A coluna " << col << "(indice " << col-P1.x << " ) tem  " << count[col-P1.x] << endl;
+            cout << "A coluna " << col << "(indice " << col-P1.x << " ) tem  " << count[col-P1.x] << endl;
         }
 
         //cout << " O quadrado " << i << " tem " << count_Den << " pontos " << endl;
-        if ( (count_Den > 10) || (control_left[i] == -1) )
+        if ( (count_Den > 10) || (control_line[i] == -1) )
         {
             if (count_Den != 0)
                 new_Center.x = count_Num / count_Den;
 
-            control_left[i] = new_Center.x;
+            control_line[i] = new_Center.x;
         }
         else
         {
-            new_Center.x = control_left[i];
-            //cout << "Centro anterior [ " << i << " ]: ( " << previous_left_Line[i].x << " , " << previous_left_Line[i].y << " ) " << endl;
-            //cout << "Centro anterior [ " << i << " ]: " << previous_left_Line[1].x << endl;
+            new_Center.x = control_line[i];
+            //cout << "Centro anterior [ " << i << " ]: ( " << previous_Central_Line[i].x << " , " << previous_Central_Line[i].y << " ) " << endl;
+            //cout << "Centro anterior [ " << i << " ]: " << previous_Central_Line[1].x << endl;
         }
 
-        left_Line.push_back(new_Center);
-        ileft_Line.push_back(new_Center); 
+        Central_Line.push_back(new_Center);
+        iCentral_Line.push_back(new_Center); 
 
         P1.x = new_Center.x - (l_rectangle/2);
         P1.y = new_Center.y - (h_rectangle/2);
@@ -512,35 +378,6 @@ void sliding_Window_Left(Mat& in, Mat& out)
     char* sliding_window = "Sliding Window";
     namedWindow(sliding_window, CV_WINDOW_NORMAL);
     imshow(sliding_window, out);
-}
-
-void navegation_Zone(Mat& in, Mat& out)
-{
-    out = in;
-
-    for (int i=0; i<right_Line.size(); i++)
-    {
-        navegavel.push_back(right_Line[i]);
-        navegavel_int.push_back(right_Line[i]);
-    }
-        
-    for (int j=left_Line.size()-1; j>=0; j--)
-    {
-        navegavel.push_back(left_Line[j]);
-        navegavel_int.push_back(left_Line[j]);
-    }
-        
-    polylines(out,navegavel_int,1,Scalar(255,0,0),2,8,0);
-
-    /*Mat draw(in.rows, in.cols, CV_8UC3, Scalar(0,0,0) );
-    fillPoly(draw, right_Line, Scalar (255, 0, 0), 8);
-    char* draw_window = "Regiao Navegavel";
-    namedWindow(draw_window, CV_WINDOW_NORMAL);
-    imshow(draw_window, draw);*/
-
-    char* navegation_window = "Regiao Navegavel";
-    namedWindow(navegation_window, CV_WINDOW_NORMAL);
-    imshow(navegation_window, out);
 }
 
 void inverse_bird_Eyes(Mat& src, Mat& in, Mat& out)
@@ -594,22 +431,8 @@ void inverse_bird_Eyes_2(Mat& src, Mat& in, Mat& out)
     polylines(out,final_navegavel_int,1,Scalar(255,0,0),3,8,0);
 }
 
-void central_Line(Mat& in)
-{
-    int x_add, y_add;
-
-    for(int i=0; i<final_navegavel.size(); i++)
-    {
-        y_add = (int) final_navegavel[i].y;
-        x_add = ( (int) final_navegavel[i].x + (int) final_navegavel[(num_rectangle*2)-i-1].x ) / 2;
-        central_line.push_back(Point(x_add,y_add));
-    }
-
-    polylines(in,central_line,0,Scalar(0,255,255),2,8,0);
-}
-
 float shift_Central(Mat& in)
-{
+{ 
     float frame_center = in.cols / 2;
     float shift;
     float shift_num = 0;
@@ -617,9 +440,9 @@ float shift_Central(Mat& in)
 
     line(in,Point(frame_center,0),Point(frame_center,in.rows),Scalar(0,255,0),3,8,0);
 
-    for(int i=0; i < central_line.size(); i++)
+    for(int i=0; i < Central_Line.size(); i++)
     {
-        shift_num  += frame_center - central_line[i].x;
+        shift_num  += frame_center - Central_Line[i].x;
         shift_den++;
     }
 
